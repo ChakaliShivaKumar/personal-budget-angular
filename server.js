bf3ded0e40eed4c61681839879244dad0b46c9b5
Shiva Kumar Chakali <schakali@charlotte.edu>
2025-02-08 13:12:17 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.use('/',express.static('public'));
-
-const budget = {
-    "myBudget": [
-        {
-            "title": "Rent",
-            "budget": 310            
-        },
-        {
-            "title": "Eat out",
-            "budget": 60
-        },
-        {
-            "title": "Grocery",
-            "budget": 160
-        },
-    ]}
 
 app.get('/hello',(req,res) =>{
     res.send("Hello World!");
 });
 
-app.get('/budget', (req,res)=>{
-    res.json(budget);
-})
+app.get('/budget', (req, res) => {
+    fs.readFile('budget-data.json', 'utf8', (err, data) => { //to read the content of the budget-data.json file
+        if (err) {
+            console.error('Error reading JSON file:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        
+        const budgetData = JSON.parse(data);
+        res.json(budgetData);
+    });
+});
 
 app.listen(port, ()=>{
     console.log(`Listening at http://localhost:${port}`);
-})
+});
